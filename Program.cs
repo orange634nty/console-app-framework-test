eba64c77baa6cd8260209627b12ccf0261fc013c
orange634nty <orange634nty@gmail.com>
2019-02-24 14:32:26 +0900

    update to ver0.3.

--- Program.cs
+++ Program.cs
@@ -7,11 +7,10 @@ using System.Threading.Tasks;
 // Entrypoint, create from the .NET Core Console App.
 class Program
 {
+    // C# 7.1(update lang version)
     static async Task Main(string[] args)
     {
-        await new HostBuilder()
-            .ConfigureLogging(x => x.AddConsole())
-            .RunBatchEngine<MyFirstBatch>(args);
+        await new HostBuilder().RunBatchEngineAsync<MyFirstBatch>(args);
     }
 }
 
@@ -19,8 +18,8 @@ class Program
 public class MyFirstBatch : BatchBase // inherit BatchBase
 {
     // allows void/Task return type, parameter allows all types(deserialized by Utf8Json and can pass by JSON string)
-    public void Hello(string name, int repeat = 99)
-    {
+    public void Hello(string name, int repeat = 3)
+    { 
         for (int i = 0; i < repeat; i++)
         {
             this.Context.Logger.LogInformation($"Hello My Batch from {name}");
