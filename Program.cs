73591ea6cfccddfc92a6604626aa256306440986
orange634nty <orange634nty@gmail.com>
2019-04-10 22:22:08 +0900

    update to ver1.0.0

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using MicroBatchFramework;
+using MicroBatchFramework;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -19,7 +19,7 @@ public class MyFirstBatch : BatchBase // inherit BatchBase
 {
     // allows void/Task return type, parameter allows all types(deserialized by Utf8Json and can pass by JSON string)
     public void Hello(string name, int repeat = 3)
-    { 
+    {
         for (int i = 0; i < repeat; i++)
         {
             this.Context.Logger.LogInformation($"Hello My Batch from {name}");
